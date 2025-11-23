f9f30f3afe3ebbb36bb905b1571415da4d961d97
ruba <retal2004retal@gmail.com>
2025-11-23 20:14:01 +0300

    تحديث1

---
+++ list.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+  <meta charset="UTF-8">
+  <title>مكتبة سراب</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <!-- شريط التنقل -->
+  <div class="navbar">
+    <ul>
+      <li><a href="#d5">اتصل بنا</a></li>
+      <li><a href="#d4">الخطة</a></li>
+      <li><a href="#d3">فريق العمل</a></li>
+      <li><a href="#d2">الكتب</a></li>
+      <li><a href="#d1">حول</a></li>
+      <li><a href="#">الرئيسية</a></li>
+      <li style="float:right;"><a href="login.php">تسجيل الدخول</a></li>
+    </ul>
+  </div>
+
+  
